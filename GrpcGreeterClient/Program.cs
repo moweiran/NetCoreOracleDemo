9313d28eafbd6f3516c06589a21e00772f3ed2a8
iamlzy@163.com <iamlzy@163.com>
2020-09-09 17:37:09 +0800

    test netcore3.1 grpc service

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,45 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcGreeter;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        { 
+            //设置允许不安全的HTTP2支持
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            // The port number(5001) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("http://localhost:50001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static GrpcChannel CreateAuthenticatedChannel(string address, string token)
+        {
+            var credentials = CallCredentials.FromInterceptor((context, metadata) =>
+            {
+                if (!string.IsNullOrEmpty(token))
+                {
+                    metadata.Add("Authorization", $"Bearer {token}");
+                }
+                return Task.CompletedTask;
+            });
+
+            // SslCredentials is used here because this channel is using TLS.
+            // CallCredentials can't be used with ChannelCredentials.Insecure on non-TLS channels.
+            var channel = GrpcChannel.ForAddress(address, new GrpcChannelOptions
+            {
+                Credentials = ChannelCredentials.Create(new SslCredentials(), credentials)
+            });
+            return channel;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -17,3 +17,17 @@ optionsBuilder.UseOracle("User Id=CARGO;Password=CARGO;Data Source=192.168.0.43:
 });
 4. 获取Sequence
 使用Oracle.ManagedDataAccess.Core+T4模版
+
+gRPC 是一种与语言无关的高性能远程过程调用 (RPC) 框架。
+gRPC 的主要优点是：
+现代高性能轻量级 RPC 框架。
+协定优先 API 开发，默认使用协议缓冲区，允许与语言无关的实现。
+可用于多种语言的工具，以生成强类型服务器和客户端。
+支持客户端、服务器和双向流式处理调用。
+使用 Protobuf 二进制序列化减少对网络的使用。
+这些优点使 gRPC 适用于：
+效率至关重要的轻量级微服务。
+需要多种语言用于开发的 Polyglot 系统。
+需要处理流式处理请求或响应的点对点实时服务。
+
+Azure 应用服务或 IIS 当前不支持 ASP.NET Core gRPC。 Http.Sys 的 HTTP/2 实现不支持 gRPC 依赖的 HTTP 响应尾随标头。
